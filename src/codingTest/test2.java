ed9a6815a07a3768b6eccc59e5ce2ad581fd3764
hojunahn <hojunahn@naver.com>
2024-04-09 17:58:38 +0900

    코테 LV 0 1번

---
+++ src/codingTest/test2.java
@@ -0,0 +1,15 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c - a;
+
+        System.out.println(b_square);
+    }
+}
